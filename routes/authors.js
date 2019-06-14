4e6ce207f2ce14afaba5220b1c6054f47bd40dac
psstory <psstory@yahoo.com>
2019-06-14 14:47:03 -0400

    Books Create/index/New

--- routes/authors.js
+++ routes/authors.js
@@ -1,19 +1,22 @@
 const express = require('express')
-const Author = require('../models/authors')
 const router = express.Router()
-// all authors route
+const Author = require('../models/author')
+
+// All Authors Route
 router.get('/', async (req, res) => {
   let searchOptions = {}
   if (req.query.name != null && req.query.name !== '') {
     searchOptions.name = new RegExp(req.query.name, 'i')
   }
-  try{  
-    const authors = await Author.find( searchOptions)
-    res.render('authors/index', {authors: authors, searchOptions: req.query})
+  try {
+    const authors = await Author.find(searchOptions)
+    res.render('authors/index', {
+      authors: authors,
+      searchOptions: req.query
+    })
   } catch {
     res.redirect('/')
   }
-  
 })
 
 // New Author Route
